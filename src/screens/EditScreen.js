a5b88a334eccdc8441faae6dcf66896b9344b80f
Anna Ionova <echoespond@gmail.com>
2019-09-17 22:25:33 -0400

    Initial setup

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>EditScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default EditScreen;
